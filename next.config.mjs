aa2d03b0a8b260d2f6e82fa12ea7be4d1a46ec07
DeenuRamenjes <deenuramenjes29@gmail.com>
2025-04-22 12:51:09 +0530

    git changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,53 +1,11 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   webpack(config) {
-//     const fileLoaderRule = config.module.rules.find((rule) =>
-//       rule.test?.test?.(".svg")
-//     );
-
-//     config.module.rules.push(
-//       {
-//         ...fileLoaderRule,
-//         test: /\.svg$/i,
-//         resourceQuery: /url/,
-//       },
-//       {
-//         test: /\.svg$/i,
-//         issuer: fileLoaderRule.issuer,
-//         resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
-//         use: {
-//           loader: "@svgr/webpack",
-//           options: {
-//             svgoConfig: {
-//               plugins: [
-//                 {
-//                   name: "preset-default",
-//                   params: {
-//                     overrides: {
-//                       removeViewBox: false,
-//                     },
-//                   },
-//                 },
-//               ],
-//             },
-//           },
-//         },
-//       }
-//     );
-
-//     fileLoaderRule.exclude = /\.svg$/i;
-
-//     return config;
-//   },
-// };
-
-// export default nextConfig;
-
-
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio-2' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio-2/' : '',
   webpack(config) {
     const fileLoaderRule = config.module.rules.find((rule) =>
       rule.test?.test?.(".svg")
@@ -62,7 +20,7 @@ const nextConfig = {
       {
         test: /\.svg$/i,
         issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...(fileLoaderRule.resourceQuery?.not || []), /url/] }, // Ensure `not` exists before spreading
+        resourceQuery: { not: [...(fileLoaderRule.resourceQuery?.not || []), /url/] },
         use: {
           loader: "@svgr/webpack",
           options: {
